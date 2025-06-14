c4c0e3482fc0d9febff50b79137b683350554e7c
Rafi <92932621+SwanLake007@users.noreply.github.com>
2025-06-14 21:55:04 +0700

    Update portal-const.js

--- src/constant/portal-const.js
+++ src/constant/portal-const.js
@@ -2,7 +2,7 @@ export const portalApps = [
   { name: 'Classroom', desc: 'Materi dan tugas', icon: '🎓', redirect_url: 'https://moodle.portal-saras.com/auth/oidc/', isForAdmin: false },
   { name: 'OneCloud', desc: 'Storage', icon: '📧', redirect_url: 'https://nextcloud.portal-saras.com/apps/user_oidc/login/1', isForAdmin: false },
   { name: 'NexaPlay', desc: 'An IPTV Service', icon: '🎞', redirect_url: 'https://iptv.portal-saras.com/sso/OID/start/Keycloak', isForAdmin: false },
-  { name: 'Chat', desc: 'A VoIP Service', icon: '🗯️', redirect_url: 'https://voip-chat-new.netlify.app', isForAdmin: false },
+  { name: 'Chat', desc: 'A VoIP Service', icon: '🗯️', redirect_url: 'https://voip.portal-saras.com', isForAdmin: false },
   { name: 'Monitoring Panel', desc: 'Grafana', icon: '📊', redirect_url: 'https://grafana.portal-saras.com', isForAdmin: true },
   { name: 'Settings', desc: 'Pengaturan', icon: '⚙️', redirect_url: 'https://google.com', isForAdmin: false },
 ]
